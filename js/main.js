157111867cb0b2708addb5608e25c1d240e7b43e
Eddie <53879350+Eddie344@users.noreply.github.com>
2020-01-12 18:59:30 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    $("#more").click(function() {
+        $(".faq__items_hide").addClass("faq__items_show");
+        $(".faq__more").addClass("faq__more_hide");
+      });
+});
